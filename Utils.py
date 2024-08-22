e8bf42f9b3be6a74e669ac94c35c608dd90dfbfd
Kate <katya700700@gmail.com>
2024-08-22 12:27:56 +0600

    fixed

--- Utils.py
+++ Utils.py
@@ -1,10 +1,11 @@
 import chess
 
+
 def getMovesToTarget(board, start, target):
     start_squares = []
     for square in chess.SQUARES:
-        if square != target and not board.occupied(square):
-            new_position = chess.square(square.file, start.rank)
-            if board.is_legal(chess.Move(new_position, target)):
+        if square != target and board.piece_at(square) is None:
+            if board.is_legal(chess.Move(square, target)):
                 start_squares += square
     return start_squares
+
