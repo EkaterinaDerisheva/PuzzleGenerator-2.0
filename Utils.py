1eb7dab789d23e1c9734e444c095a9e8086ca85c
Kate <katya700700@gmail.com>
2024-08-26 19:02:00 +0600

    finally fully working getMovesToTarget

--- Utils.py
+++ Utils.py
@@ -3,9 +3,13 @@ import chess
 
 def getMovesToTarget(board, start, target):
     start_squares = []
-    for square in chess.SQUARES:
-        if square != target and board.piece_at(square) is None:
-            if board.is_legal(chess.Move(square, target)):
-                start_squares += square
+    new_board = board.copy()
+    for square in range(64):
+        if square != chess.parse_square(target) and square != chess.parse_square(start) and new_board.piece_at(square) is None:
+            piece = new_board.remove_piece_at(chess.parse_square(start))
+            new_board.set_piece_at(square, piece)
+            if new_board.is_legal(chess.Move.from_uci(chess.square_name(square) + target)):
+                start_squares.append(square)
+            new_board = board.copy()
     return start_squares
 
